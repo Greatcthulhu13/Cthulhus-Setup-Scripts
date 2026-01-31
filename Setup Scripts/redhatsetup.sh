f51f684bdf5a42be43bb9fcf720ad4e852d619cd
Cthulhu <144727889+Greatcthulhu13@users.noreply.github.com>
2026-01-31 15:33:11 +0000

    Update redhatsetup.sh

--- Setup Scripts/redhatsetup.sh
+++ Setup Scripts/redhatsetup.sh
@@ -10,7 +10,7 @@ sudo dnf copr enable derisis13/ani-cli
 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf install -y git fastfetch lolcat winehq-stable firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2c yt-dlp ffmpeg fzf patch ani-skip timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice mu
+sudo dnf install -y git fastfetch lolcat winehq-stable firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2c yt-dlp ffmpeg fzf patch ani-skip timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice mu vlc gimp
 
 # Configure preferences 
 echo "alias ll='ls -alF'" >> ~/.bashrc
