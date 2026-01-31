77c4956fe9fe6cb55850f92015221caa55a03049
Cthulhu <144727889+Greatcthulhu13@users.noreply.github.com>
2026-01-31 15:43:26 +0000

    Update debiansetup.sh

--- Setup Scripts/debiansetup.sh
+++ Setup Scripts/debiansetup.sh
@@ -8,7 +8,7 @@ sudo apt config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/39/w
 
 sudo apt install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-sudo apt install -y git lolcat winehq-stable ani-cli firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2c ani-skip yt-dlp ffmpeg fzf patch timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice mu
+sudo apt install -y git lolcat winehq-stable ani-cli firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2c ani-skip yt-dlp ffmpeg fzf patch timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice mu gimp
 
 # Configure preferences
 echo "alias ll='ls -alF'" >> ~/.bashrc
